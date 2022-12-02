07a453fe947e37fb980789d574995ca0a47c3e4f
Anastasija Demerdjieva <anastasija.demerdjieva@tum.de>
2022-12-02 11:05:57 +0100

    first version of the function compute_costate_rev.m

--- mechanical_system/compute_costate_rev.m
+++ mechanical_system/compute_costate_rev.m
@@ -1,10 +1,11 @@
-function p = compute_costate_rev(x, u, g)
+function p = compute_costate_rev(x, u, M)
 % COMPUTE_COSTATE_REV  Compute the co-state of the reverse system 
 %                      x'(t) = -f(x(t))-g(x(t))u(t)
-   dVdX = zeros(size(u));
-   for t = 1:size(x, 1) % number of samples
-        dVdX(t,:) = 2*inv(g(x(t,:)))'*u(t,:)';
+
+   dVdq_dot = zeros(size(u));
+   for t = 1:size(u, 1) % samples
+        dVdq_dot(t,:) = 2*M(x(t,:))*u(t,:)';
    end
-   p = dVdX;
+   p = dVdq_dot';
    %p = p';
 end
