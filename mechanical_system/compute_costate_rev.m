2d7eba353035fc0949cf267f64f04b63e6259d65
Anastasija Demerdjieva <anastasija.demerdjieva@tum.de>
2022-12-09 16:21:27 +0100

    commited a correct version (the previous one had two versions in one file)

--- mechanical_system/compute_costate_rev.m
+++ mechanical_system/compute_costate_rev.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function p = compute_costate_rev(x, u, M)
 % COMPUTE_COSTATE_REV  Compute the co-state of the reverse system 
 %                      x'(t) = -f(x(t))-g(x(t))u(t)
@@ -9,16 +8,4 @@ function p = compute_costate_rev(x, u, M)
    end
    p = dVdq_dot;
    %p = p';
-=======
-function p = compute_costate_rev(x, u, M)
-% COMPUTE_COSTATE_REV  Compute the co-state of the reverse system 
-%                      x'(t) = -f(x(t))-g(x(t))u(t)
-
-   dVdq_dot = zeros(size(u));
-   for t = 1:size(u, 2) % samples
-        dVdq_dot(t,:) = 2*M(x(t,:))*u(t,:)';
-   end
-   p = dVdq_dot;
-   %p = p';
->>>>>>> 3d1dc6513b9cae82f960ee602ea761c6e32b79f5
 end
